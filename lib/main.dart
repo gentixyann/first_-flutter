1f696cadd7d63cddbff7b59918afb6c1e799948f
Gento <a551.return@gmail.com>
2020-06-29 19:10:29 +0900

    function 成功

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,35 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('押されましたね');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = ['何が食べたい？', 'どこに行きたい？'];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First....'),
-        ),
-        body: Text('This is my first body!!!'),
-      ),
+          appBar: AppBar(
+            title: Text('My First....'),
+          ),
+          body: Column(
+            children: [
+              Text('The question is'),
+              RaisedButton(
+                child: Text('Answer1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer2'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer3'),
+                onPressed: answerQuestion,
+              ),
+            ],
+          )),
     );
   }
 }

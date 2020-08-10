aff5e7515eb7f2b7c10e8bb3ed6f2e32c76223e5
Gento <a551.return@gmail.com>
2020-08-10 22:47:25 +0900

    transactiont作った

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,36 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shose', amount: 88.99, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New love', amount: 48.99, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
-      body: Center(
-        child: Text('Widget Playground'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Colors.blue,
+              child: Text('chart!!'),
+              elevation: 5,
+            ),
+          ),
+          Card(
+            color: Colors.red,
+            child: Text('List of text'),
+          )
+        ],
       ),
     );
   }

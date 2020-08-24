eca80a1cb879df15565ab97e21225396ca53eebe
Gento <a551.return@gmail.com>
2020-08-24 14:44:15 +0900

    floadting button追加

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -44,6 +50,8 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.add), onPressed: () {}),
     );
   }
 }

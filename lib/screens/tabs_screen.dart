e237827a7846124aefc99690c06a5dd77e832892
Gento <a551.return@gmail.com>
2021-02-11 11:09:55 +0900

    tabをbottomに変更

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,29 +8,34 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabsScreen> {
+  final List<Widget> _pages = [
+    CategorieSscreen(),
+    FavoritesScreen(),
+  ];
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categories',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'favorites',
-              )
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[CategorieSscreen(), FavoritesScreen()],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Theme.of(context).primaryColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), title: Text('Categories')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star), title: Text('Favorites')),
+        ],
       ),
     );
   }

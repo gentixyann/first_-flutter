6eec1d772438a86e39e8f66dc7465229056a8122
Gento <a551.return@gmail.com>
2021-02-18 21:57:20 +0900

    showAllとshowFavoritesOnlyに切り替えできた

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../widgets/products_grid.dart';
+import '../providers/products.dart';
+
+enum FilterOptions {
+  Favorites,
+  All,
+}
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: Text('MyShop'),
         actions: <Widget>[
           PopupMenuButton(
-              onSelected: (int selectedValue) {
-                print(selectedValue);
+              onSelected: (FilterOptions selectedValue) {
+                if (selectedValue == FilterOptions.Favorites) {
+                  productsContainer.showFavoritesOnly();
+                } else {
+                  productsContainer.showAll();
+                }
               },
               icon: Icon(
                 Icons.more_vert,
@@ -19,11 +31,11 @@ class ProductOverviewScreen extends StatelessWidget {
               itemBuilder: (_) => [
                     PopupMenuItem(
                       child: Text('Only Favorite'),
-                      value: 0,
+                      value: FilterOptions.Favorites,
                     ),
                     PopupMenuItem(
                       child: Text('Show All'),
-                      value: 1,
+                      value: FilterOptions.All,
                     ),
                   ])
         ],

08685b21f6031128e81b811c0e6f3870b47e1289
Gento <a551.return@gmail.com>
2021-02-21 14:08:45 +0900

    orders screenを作成

--- lib/screens/product_overview_screen.dart
+++ lib/screens/product_overview_screen.dart
@@ -5,6 +5,7 @@ import '../widgets/products_grid.dart';
 import '../widgets/badge.dart';
 import '../providers/cart.dart';
 import './cart_screen.dart';
+import '../widgets/app_drawer.dart';
 
 enum FilterOptions {
   Favorites,
@@ -64,6 +65,7 @@ class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showFavoritesOnly),
     );
   }

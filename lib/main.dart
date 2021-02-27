b9cb6cb760e19da0c0a67b0624e2ec2d3045b88e
Gento <a551.return@gmail.com>
2021-02-27 18:56:46 +0900

    edit_product_screenの原型を作成

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import './providers/cart.dart';
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
 import './screens/user_products_screen.dart';
+import './screens/edit_product_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -39,6 +40,7 @@ class MyApp extends StatelessWidget {
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
           UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+          EditProductScreen.routeName: (ctx) => EditProductScreen(),
         },
       ),
     );

a8b6775a2d139f3d22e79ca6622738acf1263f2e
Gento <a551.return@gmail.com>
2021-05-15 13:06:14 +0900

    httpsに更新

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -126,8 +126,7 @@ class Products with ChangeNotifier {
   Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
-      final url =
-          Uri.https('flutter-update.firebaseio.com', '/products/$id.json');
+      final url = Uri.https('flutter-update.firebaseio.com', '/products/$id.json');
       await http.patch(url,
           body: json.encode({
             'title': newProduct.title,
@@ -143,8 +142,7 @@ class Products with ChangeNotifier {
   }
 
   Future<void> deleteProduct(String id) async {
-    final url =
-        Uri.https('flutter-update.firebaseio.com', '/products/$id.json');
+    final url = Uri.https('flutter-update.firebaseio.com', '/products/$id.json');
     final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
     var existingProduct = _items[existingProductIndex];
     _items.removeAt(existingProductIndex);
